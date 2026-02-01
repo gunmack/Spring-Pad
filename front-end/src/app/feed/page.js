08207a32640e2c3101596580f5d2f61562f4ba16
gunmack <jreedoy2002@yahoo.com>
2026-02-01 04:11:28 -0800

    added api route for sending input

--- front-end/src/app/feed/page.js
+++ front-end/src/app/feed/page.js
@@ -16,9 +16,28 @@ export default function Feed() {
     if (!loading && !user) router.push("/auth");
   }, [loading, user, router]);
 
-  const handleAdd = (data) => {
-    console.log("New data:", data);
-    // send to backend / Firebase later
+  const handleAdd = async (data) => {
+    try {
+      const response = await fetch("/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      console.log("Status:", response.status);
+      console.log("OK:", response.ok);
+
+      const text = await response.text();
+      console.log("Response body:", text);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+    setOpenModal(false);
   };
 
   return (
